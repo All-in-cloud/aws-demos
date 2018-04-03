08dfd11af37b3f9b0372b82bd793a566f18f3a58
David Kubec <davesade.42@gmail.com>
2018-04-03 15:22:59 +0200

    Terraform examples added.

---
+++ Terraform/inputs.tf
@@ -0,0 +1,27 @@
+provider "aws" {
+  region = "eu-central-1"
+}
+
+### Find an AMI to use for the deployment
+data "aws_ami" "amazonlinux" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["amzn-ami-hvm-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["137112412989"] # Amazon
+}
+
+### The Subnet to deploy within.
+variable "SUBNET" {
+  type        = "string"
+  description = "(Mandatory) Subnet to deploy within."
+  default     = "subnet-026e8193a76e08618"
+}
---
+++ Terraform/main.tf
@@ -0,0 +1,15 @@
+### Deploy a resource of type "aws_instance"" with the logical name "Web".
+### We use the data value of the ami id found in inputs.tf, and the subnet_id defined in inputs.tf
+
+resource "aws_instance" "web" {
+  ami           = "${data.aws_ami.amazonlinux.id}"
+  instance_type = "t2.micro"
+  subnet_id     = "${var.SUBNET}"
+  key_name      = "zu697-frankfurt"
+  associate_public_ip_address = "true"
+  vpc_security_group_ids = ["sg-0fcb64bc759982301"]
+
+  tags {
+    Name = "HelloWorld"
+  }
+}
