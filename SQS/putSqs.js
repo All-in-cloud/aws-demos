3677b687faf10912afa18d4d5daff945474d16ea
David Kubec <davesade.42@gmail.com>
2018-05-22 23:09:58 +0200

    New SQS demo added.

---
+++ SQS/putSqs.js
@@ -0,0 +1,31 @@
+let AWS = require("aws-sdk");
+
+AWS.config.update({region: 'eu-central-1'});
+
+let sqs = new AWS.SQS();
+let sentMessages = 0;
+let queueURL = '<<URL_OF_SQS>>';
+
+setInterval(floodQueue,0);
+
+function floodQueue(){
+  function randomString(length) {
+    let possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    let text = "";
+    for (let i = 0; i < length; i++)
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+    return text;
+  }
+
+  let params = {
+    MessageBody: randomString(100), /* required */
+    QueueUrl: queueURL /* required */
+  };
+
+  sentMessages++;
+
+  sqs.sendMessage(params, function(err, data) {
+    if (err) console.log(err, err.stack); // an error occurred
+    else     console.log('Sent messages:', sentMessages);           // successful response
+  });
+}
